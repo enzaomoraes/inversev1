3e25438c51849e8889b2a344fbe05ed1b33af70e
enzaomoraes <enzaomoraes@usp.br>
2024-11-27 17:03:23 -0300

    v4

--- check_db.py
+++
@@ -1,8 +0,0 @@
-@app.route('/reset_db', methods=['POST'])
-def reset_db():
-    # Deletar o banco de dados
-    db.drop_all()
-    # Criar um novo banco de dados
-    db.create_all()
-    flash("Banco de dados foi resetado com sucesso!")
-    return redirect(url_for('home'))  # Redireciona para a página inicial
